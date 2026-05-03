9a7b0c42410d07aa55794a6891122b63961e302c
Yevhenii Hrebenshchykov <yevheniihrebenshchykov@gmail.com>
2026-05-03 15:45:00 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts">
+import '/src/App.css';
+</script>
+
+<template></template>
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { createPinia } from 'pinia';
+import { createApp } from 'vue';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
+
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(Toast, {
+	transition: 'Vue-Toastification__bounce',
+	maxToasts: 20,
+	newestOnTop: true,
+});
+
+app.mount('#app');
