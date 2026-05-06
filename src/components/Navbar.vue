915ce1d790fa00d41594c07c22242be4cf24e749
Yevhenii Hrebenshchykov <yevheniihrebenshchykov@gmail.com>
2026-05-06 11:50:23 +0300

    added navbar changing behavior on different screens

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,7 @@ import { CheckBadgeIcon } from '@heroicons/vue/24/outline';
 
 <template>
 	<!-- desktop -->
-	<header class="text-slate-900 bg-slate-50 hidden">
+	<header class="text-slate-900 bg-slate-50 max-md:hidden">
 		<nav class="flex justify-between items-center p-4">
 			<div class="flex gap-2 w-1/4">
 				<CheckBadgeIcon class="size-8 text-main" />
@@ -26,15 +26,19 @@ import { CheckBadgeIcon } from '@heroicons/vue/24/outline';
 	</header>
 
 	<!-- mobile -->
-	<div class="fixed w-full bottom-0 border-t border-slate-300">
-		<ul class="flex justify-around leading-10 text-center py-2">
-			<li class="w-1/3"><a href="/">Dashboard</a></li>
-			<li class="w-1/3 shrink-0">
-				<button class="cursor-pointer bg-main text-white rounded-md w-full">
-					+ Add
-				</button>
-			</li>
-			<li class="w-1/3"><a href="/stats">Stats</a></li>
-		</ul>
+	<div class="fixed w-full bottom-0 border-t border-slate-300 md:hidden">
+		<nav>
+			<ul class="flex justify-around items-center leading-10 text-center">
+				<li class="w-1/3"><a href="/">Dashboard</a></li>
+				<li class="w-1/3 shrink-0">
+					<button
+						class="cursor-pointer bg-main text-white rounded-full w-full h-12"
+					>
+						+ Add
+					</button>
+				</li>
+				<li class="w-1/3"><a href="/stats">Stats</a></li>
+			</ul>
+		</nav>
 	</div>
 </template>
